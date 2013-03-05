103167d1eafdaf735763ffd1ddae4b68cf556f07
funcoding <funcoding@funcoding4all.(none)>
2013-03-05 08:04:26 +0530

    Updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,16 +18,18 @@ class HomeController < ApplicationController
   
   protected
   def choose_word
-    choose = (2 .. 4).to_a.sample
-    @word_choose = Word.select("value").where("id = ?",choose)
+    choose = (2 .. 10).to_a.sample #A number is selected in random between 2 to 10
+    @word_choose = Word.select("value").where("id = ?",choose) #Appropiate word is fetched from database
     @word_array = {}
     @word_to_display = {}
     @word_choose.each do |word_choose|
       @word_choose = word_choose.value
     end
+    i=0
     @word_choose.split("").each do |keys|
-      @word_array[keys]=0
-      @word_to_display[keys]="_"
+      @word_array[i]=keys
+      @word_to_display[i]="_"
+      i=i+1
     end
     session[:count] = 0
     session[:word] = @word_array
@@ -39,9 +41,8 @@ class HomeController < ApplicationController
     if session[:count] < 5
       result = false
       session[:word].each do |key, value|
-        if key == letter
-          session[:word][key]=1
-          session[:word_to_display][key]=key
+        if value == letter
+          session[:word_to_display][key]=value
           result = true
         end
       end
